0a4c511513d1d107ce23b8c84cbec47eda2e699b
malachieborohoul <malachieborohoul@gmail.com>
2024-07-07 00:56:04 +0100

    Create an abstract failure in core/errors and utils/typedef that takes generic ReturnFuture

---
+++ lib/core/utils/typedef.dart
@@ -0,0 +1,4 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_tutorial/core/errors/failure.dart';
+
+typedef ResultFuture<T> = Future<Either<Failure, T>>;
--- lib/features/authentication/domain/repositories/authentication_repository.dart
+++ lib/features/authentication/domain/repositories/authentication_repository.dart
@@ -1,12 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_tutorial/core/errors/failure.dart';
 import 'package:tdd_tutorial/features/authentication/domain/entities/user.dart';
 
 abstract class AuthenticationRepository {
   const AuthenticationRepository();
 
-  Future<void> createUser(
+  Future<Either<Failure, void>> createUser(
       {required String createdAt,
       required String name,
       required String avatar});
 
-  Future<List<User>> getUsers();
+  Future<Either<Failure, List<User>>> getUsers();
 }
