c03aa44a9331a36dc4de468e81730bf041deb761
malachieborohoul <malachieborohoul@gmail.com>
2024-07-07 14:25:05 +0100

    Create UsecaseWithParams & UsecaseWithoutParams interface that is extended in different usecases

--- lib/features/authentication/domain/repositories/authentication_repository.dart
+++ lib/features/authentication/domain/repositories/authentication_repository.dart
@@ -1,14 +1,13 @@
-import 'package:dartz/dartz.dart';
-import 'package:tdd_tutorial/core/errors/failure.dart';
+import 'package:tdd_tutorial/core/utils/typedef.dart';
 import 'package:tdd_tutorial/features/authentication/domain/entities/user.dart';
 
 abstract class AuthenticationRepository {
   const AuthenticationRepository();
 
-  Future<Either<Failure, void>> createUser(
+  ResultVoid createUser(
       {required String createdAt,
       required String name,
       required String avatar});
 
-  Future<Either<Failure, List<User>>> getUsers();
+  ResultFuture<List<User>> getUsers();
 }
