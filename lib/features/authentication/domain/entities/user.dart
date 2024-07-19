4fe6f8ee3628ab2b6e91534e71898411fd77248d
malachieborohoul <malachieborohoul@gmail.com>
2024-07-19 15:23:30 +0100

    CREATE domain layer with user entity and authentication_repository Create abstract Failure class and Either functional programming feature Typedef to bind Either

--- lib/features/authentication/domain/entities/user.dart
+++ lib/features/authentication/domain/entities/user.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
@@ -6,13 +5,10 @@ class User extends Equatable {
   final String createdAt;
   final String name;
   final String avatar;
-  const User({
-    required this.id,
-    required this.createdAt,
-    required this.name,
-    required this.avatar,
-  });
+
+  const User({required this.id, required this.createdAt, required this.name, required this.avatar});
   
   @override
   List<Object?> get props => [id];
+
 }
