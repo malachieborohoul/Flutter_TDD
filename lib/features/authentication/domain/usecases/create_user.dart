08e77bd184e34070521e54d54e28cb177c25a0af
malachieborohoul <malachieborohoul@gmail.com>
2024-07-19 16:50:57 +0100

    Implement all use cases

---
+++ lib/features/authentication/domain/usecases/create_user.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:tdd_tutorial/core/usecase/usecase.dart';
+import 'package:tdd_tutorial/core/utils/typedef.dart';
+import 'package:tdd_tutorial/features/authentication/domain/repositories/authentication_repository.dart';
+
+class CreateUser extends UsecaseWithParams<void, CreateUserParams> {
+  final AuthenticationRepository _repository;
+
+  CreateUser(this._repository);
+
+  
+  @override
+  ResultFuture call(params) async => _repository.createUser(
+      createdAt: params.createdAt, name: params.name, avatar: params.avatar);
+}
+
+class CreateUserParams extends Equatable {
+  final String createdAt;
+  final String name;
+  final String avatar;
+  const CreateUserParams({
+    required this.createdAt,
+    required this.name,
+    required this.avatar,
+  });
+  @override
+  List<Object?> get props => [createdAt, name, avatar];
+}
