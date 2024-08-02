52cad2db176f9c0a5edf85d0bf40bdf05a5caa12
malachieborohoul <malachieborohoul@gmail.com>
2024-08-02 11:45:32 +0100

    Test fromMap, fromJson, toMap, toJson without dependencies

---
+++ test/features/authentication/data/models/user_model_test.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_tutorial/core/utils/typedef.dart';
+import 'package:tdd_tutorial/features/authentication/data/models/user_model.dart';
+import 'package:tdd_tutorial/features/authentication/domain/entities/user.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tModel = UserModel.empty();
+  test('should be a subclass of [User] entity', () {
+    //Arrange
+    //Assert
+    expect(tModel, isA<User>());
+  });
+  final tJson = fixture('user.json');
+  final tMap = jsonDecode(tJson) as DataMap;
+
+  group('fromMap', () {
+    test('should return a [UserModel] with the right data', () {
+      //Arrange
+
+      //Act
+      final result = UserModel.fromMap(tMap);
+      //Assert
+      expect(result, equals(tModel));
+    });
+  });
+
+  group('fromJson', () {
+    test('should return a [UserModel] with the right data', () {
+      //Arrange
+      //Act
+      final result = UserModel.fromJson(tJson);
+      //Assert
+      expect(result, equals(tModel));
+    });
+  });
+
+  group('toMap', () {
+    test('should return a [Map] with the right data ', () {
+      //Arrange
+      //Act
+      final result = tModel.toMap();
+      //Assert
+      expect(result, tMap);
+    });
+  });
+
+  group('toJson', () {
+    test('should return a [Json] with the right data', () {
+      final result = tModel.toJson();
+
+      final tJson = jsonEncode({
+        "id": "1",
+        "createdAt": "_empty.createdAt",
+        "name": "_empty.name",
+        "avatar": "_empty.avatar"
+      });
+
+      expect(result, tJson);
+    });
+  });
+
+  group('copyWith', () {});
+}
